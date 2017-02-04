6b2b423aa882fc16e689a2f72afde1348c6426ae
keegan ridgley <keegan.cabin@gmail.com>
2017-02-04 07:31:25 -0700

    converting to new controller

--- server.js
+++ server.js
@@ -8,12 +8,8 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(express.static(path.join(__dirname, '/../', 'node_modules')))
-app.use('/questions', questions);
 
-// app.use('*', function(req, res, next) {
-//   res.sendFile('index.html', {root: path.join(__dirname, 'public')})
-// })
+app.use('/questions', questions);
 
 
 
