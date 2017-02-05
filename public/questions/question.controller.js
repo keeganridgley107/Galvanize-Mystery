164a430bb38cbdbb670078f974426ea003c941a9
keegan ridgley <keegan.cabin@gmail.com>
2017-02-05 14:52:47 -0800

    finished with basic wireframe for questions page

--- public/questions/question.controller.js
+++ public/questions/question.controller.js
@@ -14,12 +14,15 @@
         const vm = this;
 
         vm.$onInit = function() {
-          console.log("onInit ng-if is yes for testing purposes");
+          console.log("onInit ALL ng-ifs are yes for testing purposes");
           vm.questionToggleFlag = true;
+          vm.finalQuestionToggleFlag = true;
 
         }; //end of oninit function
 
-
+        vm.questionToggle = function(){
+          vm.counter = 0; 
+        }
 
 
 
