32a6a4277ff74d6aa83a2b2e0bd35e602c25dfdb
keegan ridgley <keegan.cabin@gmail.com>
2017-02-05 15:50:43 -0800

    fixed routes file to work with new db backend

--- public/questions/question.controller.js
+++ public/questions/question.controller.js
@@ -12,17 +12,24 @@
 
     function questionCtrl($http) {
         const vm = this;
+        vm.data = $http.get('/questions').then(function(response){
+          console.log(response.data);
+        });
 
         vm.$onInit = function() {
           console.log("onInit ALL ng-ifs are yes for testing purposes");
           vm.questionToggleFlag = true;
           vm.finalQuestionToggleFlag = true;
 
+          $http.get('/questions').then(function(response){
+          console.log("array of questions", response.data);
+        });
+
         }; //end of oninit function
 
         vm.questionToggle = function(){
-          vm.counter = 0; 
-        }
+          vm.counter = 0;
+        };
 
 
 
