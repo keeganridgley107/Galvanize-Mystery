bdaba4d00457d03c4efb74e37641e0212935418a
keegan ridgley <keegan.cabin@gmail.com>
2017-02-06 15:46:26 -0800

    all submit forms wired up and passing info to ctrl

--- public/questions/question.controller.js
+++ public/questions/question.controller.js
@@ -16,36 +16,42 @@
 
 
         vm.$onInit = function() {
-          vm.questionToggleFlag = true;
-          vm.finalQuestionToggleFlag = true;
+            vm.questionToggleFlag = true;
+            vm.finalQuestionToggleFlag = true;
 
-          $http.get('/questions')
-          .then(function(response)
-          {
-          vm.allQuestions = response.data;
-          console.log(vm.allQuestions);
+            $http.get('/questions')
+                .then(function(response) {
+                    vm.allQuestions = response.data;
+                    console.log(vm.allQuestions);
 
-        });
+                });
 
-        $http.get('/final_question')
-        .then(function(response)
-        {
-        vm.finalQuestion = response.data;
-        console.log(vm.finalQuestion);
+            $http.get('/final_question')
+                .then(function(response) {
+                    vm.finalQuestion = response.data;
+                    console.log(vm.finalQuestion);
 
-        });
+                });
 
         }; //end of oninit function
 
 
-        vm.questionToggle = function(){
-          vm.counter = 0;
+        vm.questionToggle = function() {
+            vm.counter = 0;
+        };
+
+        vm.questionSubmit =function(question){
+          vm.counter++;
+
+          console.log(vm.questionAnswer, question.answer1);
+        };
+        vm.finalSubmit = function(){
+            console.log(vm.finalAnswer, vm.finalQuestion[0].answer1);
         };
 
 
 
 
-
-     } //end QuestionController
+    } //end QuestionController
 
 })();
