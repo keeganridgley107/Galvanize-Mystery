aa19e80a8d59d33bce32bf75466154bead01cad9
keegan ridgley <keegan.cabin@gmail.com>
2017-02-07 12:17:36 -0800

    finished with button, adding push notifications using pusher

--- public/navbar/navbar.controller.js
+++ public/navbar/navbar.controller.js
@@ -14,7 +14,14 @@
         const vm = this;
 
         vm.$onInit = function() {
-            
+
+        }
+        vm.userLoginVerify = function(){
+          console.log(vm.userLogin);
+
+          //get req to users route goes here
+          
+          //if login info === user verify === true
         }
       }
 })();
