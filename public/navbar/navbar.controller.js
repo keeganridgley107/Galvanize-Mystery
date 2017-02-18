aa1bd077471845f4e1bb99fa047c9a4141a2686b
keegan ridgley <keegan.cabin@gmail.com>
2017-02-18 11:36:41 -0700

    removing things from the nav to make it work better on mobile

--- public/navbar/navbar.controller.js
+++ public/navbar/navbar.controller.js
@@ -15,6 +15,7 @@
 
         vm.$onInit = function() {
           vm.loginFlag = false;
+          vm.adminFlag = false;
         }
         vm.userLoginVerify = function(){
           console.log(vm.userLogin);
